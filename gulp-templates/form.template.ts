76c3cb37c30e941dc5f38525c275a69e6305fee8
Philip Plant <philip.d.plant@gmail.com>
2017-08-13 00:49:16 +1200

    wrote gulp file to generate components from template, added form template. Installed a few dependencies

---
+++ gulp-templates/form.template.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
+
+@Component({
+    selector: '<%= name %>-<%= type %>',
+    templateUrl: '<%= name %>.<%= type %>.html',
+    styleUrls: ['<%= name %>.<%= type %>.css']
+})
+export class <%= className %> {
+
+    creditCardForm: FormGroup;
+
+    constructor(formBuilder:FormBuilder){
+        this.createForm();
+    }
+
+    private createForm(){
+
+    }
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,60 @@
 var gulp = require('gulp');
+var argv = require('yargs').argv;
+const template = require('gulp-template');
+var rename = require("gulp-rename");
 
 gulp.task('generate', () => {
-    console.log("component generated");
-})
+
+    var type = argv.type;
+    var name = argv.name;
+
+    saveUrl = 'src/app/' + type + 's/' + name;
+
+    templateUrl = 'gulp-templates/';
+
+    switch(type){
+        case 'form':
+           templateUrl += 'form.template.ts';
+        break;
+        case 'part':
+            templateUrl += 'part.template.ts';
+        break;
+        case 'view':
+            templateUrl += 'view.template.ts';
+        break;
+        default:
+            error('ERROR','No corresponding type for chosen template');
+            return;
+    }
+
+    console.log("generating using template:" + templateUrl);
+
+    return gulp.src(templateUrl)
+    .pipe(template({name: name, className: generateClassName(name,type), type: type}))
+    .pipe(rename(name + '.' + type + '.ts'))
+    .pipe(gulp.dest(saveUrl));
+
+});
+
+function generateClassName(name,type){
+    nameParts = name.split("-");
+    className = "";
+    for( part of nameParts){
+        className += capitalize(part);
+    }
+    className += capitalize(type);
+
+    return className;
+}
+
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function warning(title,message){
+    console.log('\x1b[33m%s\x1b[0m:',title,message);
+}
+
+function error(title,message){
+    console.log('\x1b[31m%s\x1b[0m:',title,message);
+}
